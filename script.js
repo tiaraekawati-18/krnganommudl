5323aea88805557d5fcca03c29a8138da6641c0f
tiaraekawati-18 <tiaraekawatiklaten@gmail.com>
2026-05-29 11:27:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ window.addEventListener("load", () => {
         }, 3000);
     }
 });
+
+// FIX ERROR HAMBURGER (AMAN)
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
